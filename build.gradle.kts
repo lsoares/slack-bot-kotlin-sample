51329140eb017ad65d6b12096e0276394cb5bc2f
Luís Soares <lsoares@newstore.com>
2022-05-19 11:51:44 +0100

    hello world v2

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,11 +14,11 @@ repositories {
 dependencies {
     implementation(kotlin("stdlib"))
 
-    implementation("org.slf4j:slf4j-simple:1.+")
+//    implementation("org.slf4j:slf4j-simple:1.+")
     implementation("com.slack.api:bolt:1.+")
     implementation("com.slack.api:bolt-servlet:1.+")
     implementation("com.slack.api:bolt-jetty:1.+")
-    implementation("com.slack.api:slack-api-model-kotlin-extension:1.+") // optional (Kotlin DSL to build replies)
+//    implementation("com.slack.api:slack-api-model-kotlin-extension:1.+") // optional (Kotlin DSL to build replies)
 }
 
 task("stage") {
